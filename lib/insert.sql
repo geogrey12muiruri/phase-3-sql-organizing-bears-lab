13733c56ab61cb88f8fde69f089e4fbbf6939841
geogrey12muiruri <greggambugua@gmail.com>
2022-11-01 11:48:09 +0300

    commit tree 43bf362c322039d742acbefa72f328bf38d15b72

---
+++ lib/insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO bears
+VALUES ('Mr.Chocolate', 55, 'Male', 'White', 'Sanguin', 0 ),
+       ('Rowdy', 55, 'Male', 'White', 'Sanguin', 0 ),
+       ('Tabitha', 55, 'Male', 'White', 'Sanguin', 0 ),
+       ('Sergeant Brown', 55, 'Male', 'White', 'Sanguin', 0 ),
+       ('Melissa', 55, 'Male', 'White', 'Sanguin', 0 ),
+       ('Grinch', 55, 'Male', 'White', 'Sanguin', 0 ),
+       ('Wendy', 55, 'Male', 'White', 'Sanguin', 0 ),
+       (NULL, 55, 'Male', 'White', 'Sanguin', 1 );
+       
+    
+
